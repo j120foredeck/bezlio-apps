9918d394c61cde7f6e0ab1ef46e04da6956742db
j120foredeck <j120foredeck@outlook.com>
2017-05-11 15:43:42 -0700

    Update Sales Rep 30d Query
    Remove data formats given ability to format angular pipe values.  Added
    notes related to purpose of the script and bezl.

--- roles/sales-manager/rep-30d-invoice-view/sql/Visual8/SalesReps.sql
+++ roles/sales-manager/rep-30d-invoice-view/sql/Visual8/SalesReps.sql
@@ -1,22 +1,32 @@
 
 
+--==== Created by Jon Kantola
+--==== RobbJack Corporation
+--==== 916-645-6045
 
-select sr.NAME, a.Last30d, b.Prior30d
+/* This query lists sales reps with a total invoice amount
+   for the current 30 day period, the prior 30 days, and
+   a percentage change value.
+*/
 
-from SALES_REP sr
+select sr.NAME
+	, a.Last30d as Last30d
+	, b.Prior30d as Prior30d
+	,(a.Last30d - b.Prior30d)/nullif(a.Last30d,0) as PerChange
+from SALES_REP sr with(nolock)
 
 	left outer join 
-	(select SALESREP_ID, '$' + PARSENAME(convert(varchar(20),convert(money,sum(TOTAL_AMOUNT)), 1),2) as Last30d, sum(TOTAL_AMOUNT) as sort
-	 from RECEIVABLE
+	(select SALESREP_ID, sum(TOTAL_AMOUNT) as Last30d
+	 from RECEIVABLE with(nolock)
 	 where INVOICE_DATE >= getdate() - 30
 	 group by SALESREP_ID
 	 ) a on a.SALESREP_ID = sr.ID
 
 	left outer join 
-	(select SALESREP_ID, '$' + PARSENAME(convert(varchar(20),convert(money,sum(TOTAL_AMOUNT)), 1),2) as Prior30d
-	 from RECEIVABLE
+	(select SALESREP_ID, sum(TOTAL_AMOUNT) as Prior30d
+	 from RECEIVABLE with(nolock)
 	 where INVOICE_DATE between getdate() - 60 and getdate() - 30
 	 group by SALESREP_ID
 	 ) b on b.SALESREP_ID = sr.ID
 
-order by a.sort desc
+order by a.Last30d desc
